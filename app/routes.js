ec1e35c6cf6b2378caf76cb5af723141d52c6026
William Palmer <william.palmer@digital.cabinet-office.gov.uk>
2022-10-25 12:37:25 +0100

    Add a basic route

--- app/routes.js
+++ app/routes.js
@@ -2,3 +2,32 @@
 const router = require('govuk-prototype-kit').requests.setupRouter()
 
 // Add your routes here
+
+router.post('/licence-select', function (req, res) {
+  var canoeOrKoala = req.body['canoe-or-koala']
+  if (canoeOrKoala == "canoe"){
+    res.redirect('/apply-canoe-licence')
+  } else {
+    res.redirect('/apply-koala-licence')
+  }
+})
+
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+router.post('/summary', function (req, res) {
+  var licenceType = req.body['licence-type']
+  var licenceStartDay = req.body['licence-start-day']
+  var licenceStartMonth = months[req.body['licence-start-month']-1]
+  var licenceStartYear = req.body['licence-start-year']
+
+  var licence_config = {
+    type: licenceType,
+    startDay: licenceStartDay,
+    startMonth: licenceStartMonth,
+    startYear: licenceStartYear
+  }
+
+  res.render('summary', {config: licence_config})
+
+})
+
