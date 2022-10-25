30fa7a57bfc840b9e349c44939dff8c619dbbc1a
William Palmer <william.palmer@digital.cabinet-office.gov.uk>
2022-10-25 11:29:07 +0100

    Initial commit

---
+++ app/filters.js
@@ -0,0 +1,2 @@
+/* eslint-disable-next-line no-unused-vars */
+const addFilter = require('govuk-prototype-kit').views.addFilter
---
+++ app/routes.js
@@ -0,0 +1,4 @@
+/* eslint-disable-next-line no-unused-vars */
+const router = require('govuk-prototype-kit').requests.setupRouter()
+
+// Add your routes here
